977a8481dfd6bc3edfd3ad9211693c4a31d5d0f5
Faviola Caballero <fabycg18@gmail.com>
2020-08-28 18:32:33 -0700

    added 10 questions

--- js/quiz.js
+++ js/quiz.js
@@ -20,8 +20,13 @@ function checkResult() {
     answer1 = document.getElementById("op2");
     answer2 = document.getElementById("op5");
     answer3 = document.getElementById("op10");
-    answer4 = document.getElementById("op14");
-    answer5 = document.getElementById("op18");
+    answer4 = document.getElementById("op15");
+    answer5 = document.getElementById("op20");
+    answer6 = document.getElementById("op21");
+    answer7 = document.getElementById("op27");
+    answer8 = document.getElementById("op32");
+    answer9 = document.getElementById("op34");
+    answer10 = document.getElementById("op37");
 
     //Checked is a property in js in which tells if the radio button is checked.
     //The if stmts checks if the correct answers are selected it should increase variable add by 1. In the end it should be a total of 5 points
@@ -78,12 +83,58 @@ function checkResult() {
         document.getElementById('chk5').style.color = "red";
     }
 
+     //answer6
+    if (answer6.checked == true) {
+        add++;
+        document.getElementById('chk6').innerHTML = "&#10004";
+        document.getElementById('chk6').style.color = "green";
+    } else {
+        document.getElementById('chk6').innerHTML = "&#10006";
+        document.getElementById('chk6').style.color = "red";
+    }
+      //answer7
+       if (answer7.checked == true) {
+        add++;
+        document.getElementById('chk7').innerHTML = "&#10004";
+        document.getElementById('chk7').style.color = "green";
+    } else {
+        document.getElementById('chk7').innerHTML = "&#10006";
+        document.getElementById('chk7').style.color = "red";
+    }
+       //answer8
+       if (answer8.checked == true) {
+        add++;
+        document.getElementById('chk8').innerHTML = "&#10004";
+        document.getElementById('chk8').style.color = "green";
+    } else {
+        document.getElementById('chk8').innerHTML = "&#10006";
+        document.getElementById('chk8').style.color = "red";
+    }
+      //answer9
+       if (answer9.checked == true) {
+        add++;
+        document.getElementById('chk9').innerHTML = "&#10004";
+        document.getElementById('chk9').style.color = "green";
+    } else {
+        document.getElementById('chk9').innerHTML = "&#10006";
+        document.getElementById('chk9').style.color = "red";
+    }
+      //answer10
+       if (answer10.checked == true) {
+        add++;
+        document.getElementById('chk10').innerHTML = "&#10004";
+        document.getElementById('chk10').style.color = "green";
+    } else {
+        document.getElementById('chk10').innerHTML = "&#10006";
+        document.getElementById('chk10').style.color = "red";
+    }
+
     // This adds the total score.
     document.getElementById("score").innerHTML = "Score:" + " " + add;
 
     /* formula for finding percentage is how many questions you got right by the add
     variable multiplied by 100 nd divided by total number of questions.*/
-    var percentage = add * 100 / 5;
+    var percentage = add * 100 / 10;
     document.getElementById('percentage').innerHTML = percentage + "%";
     document.getElementById('percentage').style.color = "green";
     if (percentage < 50) {
